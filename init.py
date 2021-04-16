365fed424fdb2542cf6381747e63089ea6002954
Kevin Londot <kevinlondot@hotmail.fr>
2021-04-16 11:38:35 +0200

    initialisation de la map ainsi que les joueurs/boite/interrupteurs

---
+++ init.py
@@ -0,0 +1,54 @@
+import curses
+import player
+import box
+import switch 
+
+def initialisationMap(filemap, stdscr):
+    stdscr.clear()
+    gameMap = []
+    with open(filemap, 'r') as file:
+        for line in file:
+            gameMap.append(line.strip())
+    for i in range(0, len(gameMap)):
+        stdscr.addstr(i, 0, gameMap[i])
+        stdscr.refresh()
+    return gameMap
+
+def initialisationPlayer(gameMap):
+    strMap = "".join(gameMap)
+    nbPlayer = strMap.count("P")
+    if (nbPlayer == 1):
+        for i in range(0, len(gameMap)):
+            for j in range(0, len(gameMap[i])):
+                if (gameMap[i][j] == "P"):
+                   return player.Player(j, i)
+    else:
+        print("1 et 1 seul joueur")
+
+def initialisationBox(gameMap):
+    strMap = "".join(gameMap)
+    nbBox = strMap.count("O")
+    boxes = []
+    if (nbBox > 0):
+        for i in range(0, len(gameMap)):
+            for j in range(0, len(gameMap[i])):
+                if (gameMap[i][j] == "O"):
+                   boxes.append(box.Box(j, i))
+        return boxes
+    else:
+        print("il faut au moins une boîte")
+
+def initialisationSwitch(gameMap):
+    strMap = "".join(gameMap)
+    nbSwitches = strMap.count("X")
+    switches = []
+    if (nbSwitches > 0):
+        for i in range(0, len(gameMap)):
+            for j in range(0, len(gameMap[i])):
+                if (gameMap[i][j] == "X"):
+                   switches.append(switch.Switch(j, i))
+                   print(switches)
+        return switches
+    else:
+        print("il faut au moins un interrupteur")
+   
--- maincurse.py
+++ maincurse.py
@@ -1,19 +1,21 @@
 import curses
 import sys
 import time
-import initmap
+import init
 import player
 import box
+import switch
 
 def main(stdscr):
-
-
     curses.curs_set(0)
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
     stdscr.clear()
-    map = initmap.initialisationMap(sys.argv[1], stdscr)
+    gameMap = init.initialisationMap(sys.argv[1], stdscr)
+    player1 = init.initialisationPlayer(gameMap)
+    boxes = init.initialisationBox(gameMap)
+    switches = init.initialisationSwitch(gameMap)
 
     while True:
         entry = stdscr.getch()
@@ -29,9 +31,7 @@ if __name__ == "__main__":
     # recuperer la carte
     if (len(sys.argv) != 2):
         sys.exit("Il faut charger une et une seule map.")
-
-    
     # on cherche ou est le p (x y)
     # joueur = Joueur(x, y)
-    player1 = player.Player(10,10)
+    # player1 = player.Player(10,10)
     curses.wrapper(main)
